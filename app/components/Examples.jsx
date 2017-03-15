c96bbe1c0bb3a0e44fc4683aeee3d71675dd1801
Marc <tromezee@gmail.com>
2017-03-15 21:52:45 +0100

    Rename Exemple page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-var Examples = () => (<div><h3>This is the example page !!</h3></div>);
+var Examples = () => (<div><h3>Examples</h3></div>);
 
 module.exports = Examples;
