87554306791bbd315d9921d0a82a70c02280e573
Marc <tromezee@gmail.com>
2017-03-16 10:01:24 +0100

    Styling the nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import {Link, IndexLink} from 'react-router';
 
-var Nav = () => {
-  return (
-    <div>
-      <h2>This is the nav</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
+var Nav = React.createClass({
+  onSearch: function (e) {
+    e.preventDefault();
+    alert('Not yet wired up.');
+  },
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="search" placeholder="Entrez une ville"></input></li>
+              <li><button type="button" className="button">Search</button></li>
+            </ul>
+          </form>
+        </div>
+      </div>
 
-  );
-};
+    );
+  }
+});
 
 module.exports = Nav;
