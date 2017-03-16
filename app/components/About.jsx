7600e843687b51b3c5ac134989a9550b8418b0ec
Marc <tromezee@gmail.com>
2017-03-16 11:20:12 +0100

    Style About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-var About = (props) => (<div><h3>About</h3><p>Welcome to About page.</p></div>);
-
+var About = (props) => {
+  return (
+    <div>
+      <h1 className="text-centered">About</h1>
+      <p>Une application destinée à vous fournir la température de la ville de votre choix.</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react/">React</a></li>
+        <li><a href="https://heroku.com">Heroku</a></li>
+      </ul>
+    </div>
+  );
+};
 module.exports = About;
