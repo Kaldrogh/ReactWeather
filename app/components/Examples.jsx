1dcd763afc4472ad9922471b3c3e89c145fadfbb
Marc <tromezee@gmail.com>
2017-03-16 10:31:20 +0100

    Modificzte

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+var {Link} = require('react-router');
 
-var Examples = () => (<div><h3>Examples</h3></div>);
+var Examples = () => {
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out.</p>
+      <ol>
+        <li>
+          <Link to="/?location=Clermont-Ferrand">Clermont-Ferrand, France</Link>
+        </li>
+        <li>
+          <Link to="/?location=New-York">New-York, USA</Link>
+        </li>
+      </ol>
+    </div>
+  );
+};
 
 module.exports = Examples;
